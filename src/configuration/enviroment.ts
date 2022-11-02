bbd6bf3d7cb5db0f714642b1ef9d98c33ae6fdab
CelsonF <celsondev@outlook.com>
2022-11-02 15:09:26 -0300

    adjust enviroment

--- src/configuration/enviroment.ts
+++ src/configuration/enviroment.ts
@@ -4,7 +4,8 @@ import * as Yup from "yup";
 
 dotenv.config();
 
-const enviromentSchema = Yup.object().shape({
+const enviromentSchema = Yup.object()
+  .shape({
     NODE_ENVIROMENT: Yup.string().required(),
     PORT: Yup.number().required(),
 
@@ -34,55 +35,55 @@ const enviromentSchema = Yup.object().shape({
     SMTP_SECRET: Yup.string(),
     SMTP_FROM: Yup.string(),
 
-    EXERCISEDB_API_KEY: Yup.string()
-}).unknown();
+    RAPID_API_KEY: Yup.string(),
+  })
+  .unknown();
 
 const enviroment = enviromentSchema.validateSync(process.env);
 
 export default {
-    node_enviroment: enviroment.NODE_ENVIROMENT,
-    port: enviroment.PORT,
+  node_enviroment: enviroment.NODE_ENVIROMENT,
+  port: enviroment.PORT,
 
-    url: {
-        frontend: enviroment.FRONTEND_URL,
-        backend: enviroment.BACKEND_URL
+  url: {
+    frontend: enviroment.FRONTEND_URL,
+    backend: enviroment.BACKEND_URL,
+  },
+
+  database: {
+    hostname: enviroment.DB_HOST,
+    port: enviroment.DB_PORT,
+    username: enviroment.DB_USER,
+    password: enviroment.DB_PASSWORD,
+    name: enviroment.DB_DATABASE,
+  },
+
+  redis: {
+    hostname: enviroment.REDIS_HOST,
+    port: enviroment.REDIS_PORT,
+    username: enviroment.REDIS_USER,
+    password: enviroment.REDIS_PASSWORD,
+  },
+
+  jwt: {
+    secret: enviroment.JWT_SECRET,
+    access: enviroment.JWT_ACCESS_EXPIRATION_TIME,
+    refresh: enviroment.JWT_REFRESH_EXPIRATION_TIME,
+    reset_password: enviroment.JWT_RESET_PASSWORD_EXPIRATION_TIME,
+    verify_email: enviroment.JWT_VERIFY_EMAIL_EXPIRATION_TIME,
+  },
+
+  smtp: {
+    host: enviroment.SMTP_HOST,
+    email: enviroment.SMTP_EMAIL,
+    password: enviroment.SMTP_PASSWORD,
+    secret: enviroment.SMTP_SECRET,
+    from: enviroment.SMTP_FROM,
+  },
+
+  api: {
+    rapidapi: {
+      key: enviroment.RAPID_API_KEY,
     },
-
-    database: {
-        hostname: enviroment.DB_HOST,
-        port: enviroment.DB_PORT,
-        username: enviroment.DB_USER,
-        password: enviroment.DB_PASSWORD,
-        name: enviroment.DB_DATABASE,
-    },
-
-    redis: {
-        hostname: enviroment.REDIS_HOST,
-        port: enviroment.REDIS_PORT,
-        username: enviroment.REDIS_USER,
-        password: enviroment.REDIS_PASSWORD
-    },
-
-    jwt: {
-        secret: enviroment.JWT_SECRET,
-        access: enviroment.JWT_ACCESS_EXPIRATION_TIME,
-        refresh: enviroment.JWT_REFRESH_EXPIRATION_TIME,
-        reset_password: enviroment.JWT_RESET_PASSWORD_EXPIRATION_TIME,
-        verify_email: enviroment.JWT_VERIFY_EMAIL_EXPIRATION_TIME
-    },
-
-    smtp: {
-        host: enviroment.SMTP_HOST,
-        email: enviroment.SMTP_EMAIL,
-        password: enviroment.SMTP_PASSWORD,
-        secret: enviroment.SMTP_SECRET,
-        from: enviroment.SMTP_FROM,
-    },
-
-    api: {
-        rapidapi: {
-            key: enviroment.EXERCISEDB_API_KEY,
-        }
-    }
+  },
 };
-
